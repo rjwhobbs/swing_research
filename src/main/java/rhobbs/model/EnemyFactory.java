5d8499d52e7997c805d77c9c15fca6972749c7ec
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-09-07 15:54:48 +0200

    Tweaks to enemy stats

--- src/main/java/rhobbs/model/EnemyFactory.java
+++ src/main/java/rhobbs/model/EnemyFactory.java
@@ -4,11 +4,11 @@ public abstract class EnemyFactory {
   public static Enemy createEnemy(String enemyType, int level) {
     switch (enemyType) {
       case "Clueless audience member":
-        return new Enemy(enemyType, level, 100,40, 50);
+        return new Enemy(enemyType, level, 110,40, 50);
       case "Venue DJ":
         return new Enemy(enemyType, level, 100, 100, 50);
       case "Drunk fan":
-        return new Enemy(enemyType, level, 50, 50, 20);
+        return new Enemy(enemyType, level, 50, 110, 20);
       case "Venue owner":
         return new Enemy(enemyType, level, 150, 100, 50);
       case "Grumpy sound engineer":
